ebc3956b450e946fd0c4046382bacf1ecd24fcbf
Dannon Baker <dannon.baker@gmail.com>
2019-11-03 09:53:19 -0500

    Incremental md integration.

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,26 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+    siteName: "Galaxy Community Hub",
+    plugins: [
+        {
+            use: '@gridsome/source-filesystem',
+            options: {
+              path: './src/content/**/*.md',
+              typeName: 'Default',
+              remark: {
+                // remark options
+              }
+            }
+          }
+        ],
+        transformers: {
+          remark: {
+            // global remark options
+          }
+        }
+};
