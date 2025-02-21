551084208ef9ffe9e301e0a091972ec95a1ac018
Dannon Baker <dannon.baker@gmail.com>
2025-02-21 09:59:19 -0500

    Remove EU filter from events and news queries

--- src/pages/bare/fr/latest/News.vue
+++ src/pages/bare/fr/latest/News.vue
@@ -27,7 +27,7 @@ export default {
 <page-query>
 query {
     news: allParentArticle(
-        limit: 5, filter: {category: {eq: "news" }, subsites: {contains: ["eu"]}, draft: {ne: true}}
+        limit: 5, filter: {category: {eq: "news" }, draft: {ne: true}}
     ) {
         totalCount
         edges {
